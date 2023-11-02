75801daf84826f718893739a15939bc53c2eb00f
azmira <your_email_address@example.com>
2023-11-02 19:00:31 +0800

    updated azurerm_vm main.tf

--- terraform-linux/azurerm_vm/main.tf
+++ terraform-linux/azurerm_vm/main.tf
@@ -1,3 +1,13 @@
+
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
 # Generate random text for a unique storage account name
 resource "random_id" "random_id" {
   keepers = {
